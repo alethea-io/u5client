0118e71fc27d4f004b0b6f29f8f4b9c7faff760b
Aleksandar Djuric <aleksandardjuric@alethea.io>
2024-06-15 13:54:14 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,94 @@
+# u5client
+
+`u5client` is a command-line interface (CLI) tool designed for interacting with UTXO RPC APIs. It allows users to fetch, dump, and follow the tip of blockchain data effectively, supporting operations like fetching specific blocks, dumping block history, and subscribing to blockchain tip updates.
+
+## Installation
+
+To install `u5client`, ensure you have Rust and Cargo installed on your machine. Clone the repository and build the project using Cargo:
+
+```bash
+git clone https://github.com/alethea-io/u5client.git
+cd u5client
+cargo build --release
+```
+
+The executable will be available under `target/release/u5client`.
+
+## Configuration
+
+Before using `u5client`, you need to set up a configuration file named `config.toml` by default, or you can specify another file name with the `--config` option when running commands. Here's an example configuration:
+
+```toml
+peer = "http://localhost:50051"
+save_dir = "/path/to/your/save/directory"
+```
+
+- `peer`: URL of the UTXO RPC API server.
+- `save_dir`: Directory where the blocks will be saved if the save option is enabled.
+
+## Usage
+
+`u5client` supports several commands:
+
+### Fetch
+
+Fetch blocks from the UTXO RPC API based on their references.
+
+**Usage**:
+
+```bash
+u5client fetch --refs <block-ref1> <block-ref2> ... [--save]
+```
+
+- `--refs`: A list of block references to fetch, formatted as `index-hash`.
+- `--save`: Optional flag. If set, fetched blocks will be saved as JSON files in the configured `save_dir`. If omitted, blocks will be printed to the console.
+
+**Example**:
+
+```bash
+u5client fetch --refs 50028-8046dce8b943dc327c04712641b2db446860afafe4602e0d779bbc2bfc5b7fc2 50030-bae8938f06f9c6f7ba366688aac6dc649567d7022fb39e3190cee44952840ef3 --save
+```
+
+### Dump
+
+Dump a series of blocks starting from a specified block reference.
+
+**Usage**:
+
+```bash
+u5client dump --ref <block-ref> --num-blocks <number> [--save]
+```
+
+- `--ref`: The starting block reference, formatted as `index-hash`.
+- `--num_blocks`: Number of blocks to dump starting from the reference.
+- `--save`: Optional flag. If set, dumped blocks will be saved as JSON files. Otherwise, they will be printed to the console.
+
+**Example**:
+
+```bash
+u5client dump --ref 50028-8046dce8b943dc327c04712641b2db446860afafe4602e0d779bbc2bfc5b7fc2 --num-blocks 10 --save
+```
+
+### Follow
+
+Subscribe to updates at the tip of the blockchain, optionally starting from specified block references.
+
+**Usage**:
+
+```bash
+u5client follow [--refs <block-ref1> <block-ref2> ...]
+```
+
+- `--refs`: Optional. Block references to establish intersection points for the follow operation.
+
+**Example**:
+
+```bash
+u5client follow
+```
+
+Each command outputs results directly to the console unless the `--save` flag is used, which diverts output to JSON files in the specified save directory.
+
+## Contributions
+
+Contributions are welcome. Please fork the repository, make your changes, and submit a pull request.
---
+++ src/main.rs
@@ -0,0 +1,195 @@
+use serde::Deserialize;
+use utxorpc_spec::utxorpc::v1alpha::sync::chain_sync_service_client::ChainSyncServiceClient;
+use utxorpc_spec::utxorpc::v1alpha::sync::BlockRef;
+use utxorpc_spec::utxorpc::v1alpha::sync::DumpHistoryRequest;
+use utxorpc_spec::utxorpc::v1alpha::sync::FetchBlockRequest;
+use utxorpc_spec::utxorpc::v1alpha::sync::FollowTipRequest;
+
+use clap::{Parser, Subcommand};
+use config::{Config, Environment, File};
+use serde_json::json;
+use tonic::Request;
+
+#[derive(Parser)]
+#[clap(
+    name = "u5client",
+    about = "Lightweight CLI tool for interacting with UTXO RPC APIs",
+    version
+)]
+struct Cli {
+    #[clap(
+        short,
+        long,
+        default_value = "config.toml",
+        help = "Path to the configuration file"
+    )]
+    config: String,
+
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Deserialize, Debug)]
+struct RootConfig {
+    peer: String,
+    save_dir: String,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Fetch {
+        #[clap(long, help = "Block references to fetch")]
+        refs: Vec<String>,
+        #[clap(long, help = "Save fetched blocks to files")]
+        save: bool,
+    },
+    Dump {
+        #[clap(long, help = "Block reference to start dumping from")]
+        r#ref: String,
+        #[clap(long, help = "Number of blocks to dump")]
+        num_blocks: u32,
+        #[clap(long, help = "Save dumped blocks to files")]
+        save: bool,
+    },
+    Follow {
+        #[clap(long, help = "Block references to try and intersect")]
+        refs: Option<Vec<String>>,
+    },
+}
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = Cli::parse();
+
+    let mut settings = Config::builder();
+    settings = settings.add_source(File::with_name(&cli.config));
+    settings = settings.add_source(Environment::with_prefix("U5CLIENT").separator("__"));
+    let config = settings.build()?.try_deserialize::<RootConfig>()?;
+
+    println!("Using configuration: {:?}", config);
+
+    match &cli.command {
+        Commands::Dump {
+            r#ref,
+            num_blocks,
+            save,
+        } => {
+            let mut client = ChainSyncServiceClient::connect(config.peer).await?;
+            let parts: Vec<&str> = r#ref.split('-').collect();
+
+            let dump_history_request = DumpHistoryRequest {
+                start_token: Some(BlockRef {
+                    index: parts[0].parse().unwrap(),
+                    hash: hex::decode(parts[1]).unwrap().into(),
+                }),
+                max_items: *num_blocks,
+                field_mask: None,
+            };
+
+            let response = client
+                .dump_history(Request::new(dump_history_request))
+                .await?
+                .into_inner();
+
+            for any_chain_block in response.block.into_iter() {
+                let (index, block_data) = match any_chain_block.chain.unwrap() {
+                    utxorpc_spec::utxorpc::v1alpha::sync::any_chain_block::Chain::Cardano(
+                        block,
+                    ) => (block.header.clone().unwrap().slot, json!(block)),
+                    _ => continue,
+                };
+
+                if *save {
+                    let filename = format!("{}/{}.json", config.save_dir, index);
+                    let mut file = std::fs::File::create(filename)?;
+                    serde_json::to_writer_pretty(&mut file, &block_data)?;
+                } else {
+                    println!("Block {}: {:?}", index, block_data);
+                }
+            }
+        }
+        Commands::Fetch { refs, save } => {
+            let mut client = ChainSyncServiceClient::connect(config.peer).await?;
+            let block_refs: Vec<BlockRef> = refs
+                .iter()
+                .map(|ref_str| {
+                    let parts: Vec<&str> = ref_str.split('-').collect();
+                    BlockRef {
+                        index: parts[0].parse().unwrap(),
+                        hash: hex::decode(parts[1]).unwrap().into(),
+                    }
+                })
+                .collect();
+
+            let request = FetchBlockRequest {
+                r#ref: block_refs,
+                field_mask: None,
+            };
+
+            let response = client
+                .fetch_block(Request::new(request))
+                .await?
+                .into_inner();
+
+            for any_chain_block in response.block.into_iter() {
+                let (index, block_data) = match any_chain_block.chain.unwrap() {
+                    utxorpc_spec::utxorpc::v1alpha::sync::any_chain_block::Chain::Cardano(
+                        block,
+                    ) => (block.header.clone().unwrap().slot, json!(block)),
+                    _ => continue,
+                };
+
+                if *save {
+                    let filename = format!("{}/{}.json", config.save_dir, index);
+                    let mut file = std::fs::File::create(filename)?;
+                    serde_json::to_writer_pretty(&mut file, &block_data)?;
+                } else {
+                    println!("Block {}: {:?}", index, block_data);
+                }
+            }
+        }
+        Commands::Follow { refs } => {
+            let mut client = ChainSyncServiceClient::connect(config.peer).await?;
+
+            let intersect_refs: Vec<BlockRef> = refs
+                .as_ref()
+                .unwrap_or(&Vec::new())
+                .iter()
+                .map(|intersect| {
+                    let parts: Vec<&str> = intersect.split('-').collect();
+                    BlockRef {
+                        index: parts[0].parse().unwrap(),
+                        hash: hex::decode(parts[1]).unwrap().into(),
+                    }
+                })
+                .collect();
+
+            let request = FollowTipRequest {
+                intersect: intersect_refs,
+            };
+
+            let mut stream = client.follow_tip(Request::new(request)).await?.into_inner();
+
+            while let Some(response) = stream.message().await? {
+                match response.action.unwrap() {
+                    utxorpc_spec::utxorpc::v1alpha::sync::follow_tip_response::Action::Apply(
+                        block,
+                    ) => {
+                        println!("Apply this block: {:?}", json!(block));
+                    }
+                    utxorpc_spec::utxorpc::v1alpha::sync::follow_tip_response::Action::Undo(
+                        block,
+                    ) => {
+                        println!("Undo this block: {:?}", json!(block));
+                    }
+                    utxorpc_spec::utxorpc::v1alpha::sync::follow_tip_response::Action::Reset(
+                        ref_block,
+                    ) => {
+                        println!("Reset to this block reference: {:?}", ref_block);
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
